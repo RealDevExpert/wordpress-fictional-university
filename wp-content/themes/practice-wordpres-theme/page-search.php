0686ff2814ab0c07a1aab8b1e372cd823c06390d
RealDevExpert <georgedotjmartinez@outlook.com>
2022-02-09 19:10:21 +0000

    Add styling

--- wp-content/themes/practice-wordpres-theme/page-search.php
+++ wp-content/themes/practice-wordpres-theme/page-search.php
@@ -51,9 +51,11 @@
 
       <div class="generic-content">
         <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-        <label for="s">Perform a new search:</label>  
-        <input type="search" name="s" id="s">
-          <input type="submit" value="Search">
+          <label class="headline headline--medium" for="s">Perform a new search:</label>
+          <article class="search-form-row">
+            <input placeholder="What are you looking for? e.g. math" class="s" type="search" name="s" id="s">
+            <input class="search-submit" type="submit" value="Search">
+          </article>
         </form>
       </div>
     </div>

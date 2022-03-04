74e9d4a0e6de5112f895fd58984b4c67a3ee886b
RealDevExpert <georgedotjmartinez@outlook.com>
2022-03-04 21:01:10 +0000

    Refactor: Change line to block comment

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -35,11 +35,19 @@ class WordCountAndTimePlugin {
     // name of the section, title of the section, content, page slug we want to add this section to
     add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
 
-    // the name of the option or setting that we want to tie this to, HTML label text, function that's responsible for actually outputting the HTML
-    // slug of this page, section you want to add this field to
+    /*
+      Parameters: 
+      The name of the option or setting that we want to tie this to, HTML label text, function that's responsible for actually outputting the HTML
+      slug of this page, section you want to add this field to
+    */
     add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
-    // group, actual name of setting, array(sanitize text field, default value)
-    // sanitize_text_field: sanitize a user's input value
+    /*
+      Parameters:
+      Group, actual name of setting, array(sanitize text field, default value)
+    */
+    /* 
+      Note:
+      sanitize_text_field: sanitize a user's input value */
     register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
 
     add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');

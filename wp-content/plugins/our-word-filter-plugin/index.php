f1f8c3c7383a925afe4642270432e5f29b5866d8
RealDevExpert <georgedotjmartinez@outlook.com>
2022-03-14 10:51:31 +0000

    Show success message when saving changes to the form

--- wp-content/plugins/our-word-filter-plugin/index.php
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -82,6 +82,7 @@ class OurWordFilterPlugin {
         <h1>Word Filter Options</h1>
         <form action="options.php" method="post">
           <?php 
+            settings_errors();
             settings_fields('replacementFields');
             do_settings_sections('word-filter-options');
             submit_button(); 

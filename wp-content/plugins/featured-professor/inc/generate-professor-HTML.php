04ca157eab7aa6d384370a348187e6aa631e1b45
RealDevExpert <georgedotjmartinez@outlook.com>
2022-03-28 20:18:31 +0100

    Add background image of the professor

--- wp-content/plugins/featured-professor/inc/generate-professor-HTML.php
+++ wp-content/plugins/featured-professor/inc/generate-professor-HTML.php
@@ -11,7 +11,7 @@
       ob_start(); ?>
       <!-- HTML here -->
       <div class="professor-callout">
-        <div class="professor-callout__photo">
+        <div class="professor-callout__photo" style="background-image: url(<?php the_post_thumbnail_url('professorPortrait') ?>)">
         <div class="professor-callout__text">
           <h5><?php the_title()?></h5>
           <p><?php echo wp_trim_words(get_the_content(), 30); ?></p>

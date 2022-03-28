8dc088b5e94906ed405460f2a5675c855d53b2ca
RealDevExpert <georgedotjmartinez@outlook.com>
2022-03-28 20:04:08 +0100

    Query professor post to show the title

--- wp-content/plugins/featured-professor/inc/generate-professor-HTML.php
+++ wp-content/plugins/featured-professor/inc/generate-professor-HTML.php
@@ -1,6 +1,27 @@
 <?php
   function generateProfessorHTML($professorID) {
-    return 'Hello from the generate-professor-HTML.php file.';
+    $professorPostQuery = new WP_Query(array(
+      'post_type' => 'professor',
+      'p' => $professorID
+    ));
+
+    while ($professorPostQuery->have_posts()) {
+      $professorPostQuery->the_post();
+
+      ob_start(); ?>
+      <!-- HTML here -->
+      <div class="professor-callout">
+        <div class="professor-callout__photo">
+        <div class="professor-callout__text">
+          <h5><?php the_title()?></h5>
+        </div>
+        </div>
+      </div>
+      <?php
+      wp_reset_postdata();
+      return ob_get_clean();
+    }
+
   }
 
 ?>

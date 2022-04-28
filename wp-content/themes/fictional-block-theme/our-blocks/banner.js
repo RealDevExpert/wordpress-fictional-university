6229164ceba9a1ee2a1271c61bce7b145b030c04
RealDevExpert <georgedotjmartinez@outlook.com>
2022-04-28 14:55:10 +0300

    Allow banner to only receive custom block types of Generic Heading

--- wp-content/themes/fictional-block-theme/our-blocks/banner.js
+++ wp-content/themes/fictional-block-theme/our-blocks/banner.js
@@ -19,7 +19,7 @@ function EditComponent(params) {
     <div className="page-banner">
       <div className="page-banner__bg-image" style={{backgroundImage: "url('/wp-content/themes/fictional-block-theme/images/library-hero.jpg')"}}></div>
       <div className="page-banner__content container t-center c-white">
-        <InnerBlocks allowedBlocks={['core/paragraph', 'core/heading', 'core/list']} />
+        <InnerBlocks allowedBlocks={['ourblocktheme/genericheading']} />
       </div>
     </div>
   )

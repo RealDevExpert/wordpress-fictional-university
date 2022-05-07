156ac7f0b2a604a431b85506c288c3ed8097cc57
RealDevExpert <georgedotjmartinez@outlook.com>
2022-05-07 13:33:47 +0100

    Add size options to the hover toolbar

--- wp-content/themes/fictional-block-theme/our-blocks/genericHeading.js
+++ wp-content/themes/fictional-block-theme/our-blocks/genericHeading.js
@@ -1,4 +1,5 @@
-import { RichText} from "@wordpress/block-editor"
+import { ToolbarGroup, ToolbarButton } from "@wordpress/components"
+import { RichText, BlockControls } from "@wordpress/block-editor"
 
 wp.blocks.registerBlockType("ourblocktheme/genericheading", {
   title: "Generic Heading",
@@ -17,6 +18,13 @@ function EditComponent(props) {
 
   return (
     <>
+      <BlockControls>
+        <ToolbarGroup>
+          <ToolbarButton isPressed={props.attributes.size === "large"} onClick={() => props.setAttributes({size: "large"})}>Large</ToolbarButton>
+          <ToolbarButton isPressed={props.attributes.size === "medium"} onClick={() => props.setAttributes({size: "medium"})}>Medium</ToolbarButton>
+          <ToolbarButton isPressed={props.attributes.size === "small"} onClick={() => props.setAttributes({size: "small"})}>Small</ToolbarButton>
+        </ToolbarGroup>
+      </BlockControls>
       <RichText allowedFormats={["core/bold"]} tagName="h1" className={`headline headline--${props.attributes.size}`} value={props.attributes.text} onChange={handleTextChange} />
     </>
   )

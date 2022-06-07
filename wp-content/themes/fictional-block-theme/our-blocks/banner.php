96c07deb7cbd5acbea4f8815a515f2e732d740e4
RealDevExpert <georgedotjmartinez@outlook.com>
2022-06-07 21:22:55 +0100

    Set default background image for the front-end
    PHP is handling that

--- wp-content/themes/fictional-block-theme/our-blocks/banner.php
+++ wp-content/themes/fictional-block-theme/our-blocks/banner.php
@@ -1,3 +1,9 @@
+<?php
+if (!$attributes['imageURL']) {
+  $attributes['imageURL'] = get_theme_file_uri(('/images/library-hero.jpg'));
+}
+?>
+
 <div class="page-banner">
   <div class="page-banner__bg-image" style="backgroundImage: url('<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>')"></div>
   <div class="page-banner__content container t-center c-white">
